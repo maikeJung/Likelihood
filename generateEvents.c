56d4c07dfe2c70d213117370d2ac1aa288eade5c
Maike Jung <maike_lauf@web.de>
2017-02-02 14:09:14 +0100

    fixed drawing random numbers- lead to wrong array access

--- generateEvents.c
+++ generateEvents.c
@@ -21,12 +21,16 @@ BINNING: defined in header-file (spectrum.h)
 #include <sys/stat.h>
 #include <errno.h>
 
-void createEvents(user_data_t mass, user_data_t distance, user_data_t events, bool triggEff, bool energyRes, int filenumber, user_data_t *spectrum, user_data_t max){
+void createEvents(user_data_t mass, user_data_t distance, user_data_t events, bool triggEff, bool energyRes, int filenumber, user_data_t *spectrum, user_data_t max, user_data_t noise){
 
-    /* file for storing time & energy */
-    char filename[sizeof "DATA/10.00Mpc_700Events_1.57eV_event_1.45eV_10.5Mpc_1000Events_real_1111.txt"];
+    /* file for storing time & energy 
+    char filename[sizeof "DATA/NOISETEST/10.00Mpc_700Events_1.57eV_event_b000_1.45eV_10.5Mpc_1000Events_real_1111.txt"];
     if (triggEff && energyRes){
-        sprintf(filename, "DATA/%.2fMpc_%.0fEvents_%.2feV/events_%.2feV_%.2fMpc_%.0fEvents_real_%d.txt",distance, events, mass, mass, distance, events, filenumber);
+        sprintf(filename, "DATA/NOISETEST/%.2fMpc_%.0fEvents_%.2feV_b%.3f/events_%.2feV_%.2fMpc_%.0fEvents_real_%d.txt",distance, events, mass, noise, mass, distance, events, filenumber);
+    }*/
+    char filename[sizeof "DATA/10.00Mpc_700Events_1.57eV_event_b000_1.454567eV_10.5Mpc_1000Events_real_1111.txt"];
+    if (triggEff && energyRes){
+        sprintf(filename, "DATA/%.2fMpc_%.0fEvents_%.5feV/events_%.2feV_%.2fMpc_%.0fEvents_real_%d.txt",distance, events, mass, mass, distance, events, filenumber);
     }
     else {
         sprintf(filename, "DATA/events_%.2feV_%.1fMpc_%.0fEvents_ideal_%d_test.txt", mass, distance, events, filenumber);
@@ -41,7 +45,7 @@ void createEvents(user_data_t mass, user_data_t distance, user_data_t events, bo
     int randE, randT;
     user_data_t randCheck;
     while(eventsGenerated < events){
-        randE = rand() % (RESE);
+        randE = rand() % (RESE-1);
         randT = rand() % (REST);
         randCheck = rand()*max/RAND_MAX;
         if (spectrum[randT*(RESE-1)+randE] >= randCheck){
@@ -69,19 +73,21 @@ int main(void){
     bool triggEff = true;
     bool energyRes = true;
     bool plot = false;
-    user_data_t mass = 0.00;
+    user_data_t mass = 0.0001;
     user_data_t distance = 1.0;
     user_data_t events = 160.0;
+    double noise_events = 2.0;
     user_data_t max;
     int filenumber, i;
     double noise_rate = pow(10,-3); //Hz, expected total noise rate
     // noise that has to be added to each bin:
     user_data_t noise = noise_rate*STEPT;
+    //user_data_t noise = 2.0;
     
     // generate spectrum from whicht time/energy events are drawn
     user_data_t spectrum[(RESE-1)*REST];
 	//double *spectrum= (double*) malloc((RESE-1) * REST * sizeof(double));
-    createSpectrum(spectrum, mass, distance, events, energyRes, triggEff, noise);
+    createSpectrum(spectrum, mass, distance, events, energyRes, triggEff, noise, noise_events);
 
     if(plot){
         /*create a file from the spectrum that can then be ploted*/
@@ -99,14 +105,18 @@ int main(void){
     srand( (unsigned)time( NULL ) );
     /*create files that contain pseudo experiments*/
     // create directory
+    /*mkdir("DATA/NOISETEST", S_IRWXU);
+    char dirname[sizeof "DATA/NOISETEST/10.00Mpc_700Events_1.57eV"];
+    sprintf(dirname, "DATA/NOISETEST/%.2fMpc_%.0fEvents_%.2feV_b%.3f", distance, events, mass, noise);
+    */
     mkdir("DATA", S_IRWXU);
-    char dirname[sizeof "DATA/10.00Mpc_700Events_1.57eV"];
-    sprintf(dirname, "DATA/%.2fMpc_%.0fEvents_%.2feV", distance, events, mass);
+    char dirname[sizeof "DATA/10.00Mpc_700Events_1.576666eV"];
+    sprintf(dirname, "DATA/%.2fMpc_%.0fEvents_%.5feV", distance, events, mass);
     mkdir(dirname, S_IRWXU);
 
     for (filenumber=1; filenumber<1001; filenumber++){ 
         printf("creating file %d \n", filenumber);
-        createEvents(mass, distance, events, triggEff, energyRes, filenumber, spectrum, max);
+        createEvents(mass, distance, events, triggEff, energyRes, filenumber, spectrum, max, noise);
     }
 
     //free(spectrum);
